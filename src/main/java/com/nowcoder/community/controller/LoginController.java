55cf676aac097262b5cc56e5dfb02b2bcc442fe5
2bhuanleduo <15717007850@163.com>
2024-03-22 08:23:40 +0800

    上传-完成发送邮件和开发注册功能

---
+++ src/main/java/com/nowcoder/community/controller/LoginController.java
@@ -0,0 +1,65 @@
+package com.nowcoder.community.controller;
+
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.UserService;
+import com.nowcoder.community.util.CommunityConstant;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Map;
+
+@Controller
+public class LoginController implements CommunityConstant {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(path = "/register", method = RequestMethod.GET)
+    public String getRegisterPage() {
+        return "/site/register";
+    }
+
+    @RequestMapping(path = "/login", method = RequestMethod.GET)
+    public String getLoginPage() {
+        return "/site/login";
+    }
+
+    @RequestMapping(path = "/register", method = RequestMethod.POST)
+    public String sendRegisterPage(Model model, User user) {
+
+        Map<String, Object> map = userService.register(user);
+        // 未返回消息，说明已经注册成功
+        if (map == null || map.isEmpty()) {
+            model.addAttribute("msg", "注册成功，我们已经向您的邮箱发送了一封激活邮件，请您尽快激活！");
+            model.addAttribute("target", "/index");
+            return "/site/operate-result";
+        } else {
+            model.addAttribute("usernameMsg", map.get("usernameMsg"));
+            model.addAttribute("emailMsg", map.get("emailMsg"));
+            model.addAttribute("passwordMsg", map.get("passwordMsg"));
+            return "/site/register";
+        }
+    }
+
+    @RequestMapping(path = "/activation/{userId}/{activationCode}", method = RequestMethod.GET)
+    public String activation(Model model, @PathVariable("userId") int userId,
+                             @PathVariable("activationCode") String activationCode) {
+        int status = userService.activation(userId, activationCode);
+        if (status == ACTIVATION_SUCCESS) {
+            model.addAttribute("msg", "激活成功，您的账号可以正常使用了！");
+            model.addAttribute("target", "/login");
+        } else if (status == ACTIVATION_REPEAT) {
+            model.addAttribute("msg", "无效操作，该账号已经激活过了！");
+            model.addAttribute("target", "/index");
+        } else {
+            model.addAttribute("msg", "激活失败，您提供的激活码不正确！");
+            model.addAttribute("target", "/index");
+        }
+        return "/site/operate-result";
+    }
+}
--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -2,17 +2,125 @@ package com.nowcoder.community.service;
 
 import com.nowcoder.community.dao.UserMapper;
 import com.nowcoder.community.entity.User;
+import com.nowcoder.community.util.CommunityConstant;
+import com.nowcoder.community.util.CommunityUtil;
+import com.nowcoder.community.util.MailClient;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 @Service
-public class UserService {
+public class UserService implements CommunityConstant {
 
     @Autowired
     private UserMapper userMapper;
+
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Value("${community.path.domain}")
+    private String domain;
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
 
     public User findUserById(int id) {
         return userMapper.selectById(id);
     }
 
+    /**
+     * 用于封装注册功能，包括1. 验证注册用户的邮箱、用户名和密码是否合法；
+     * 2. 向数据库插入数据；
+     * 3. 生成激活链接
+     * @param user
+     * @return
+     */
+    public Map<String, Object> register(User user) {
+        Map<String, Object> map = new HashMap<>();
+
+        // 对空值进行判断
+        if (user == null) {
+            throw new IllegalArgumentException("参数不能为空！");
+        }
+        // 用户名 空值判断
+        if (StringUtils.isBlank(user.getUsername())) {
+            map.put("usernameMsg", "用户名不能为空！");
+            return map;
+        }
+        // 密码 空值判断
+        if (StringUtils.isBlank(user.getPassword())) {
+            map.put("passwordMsg", "密码不能为空！");
+            return map;
+        }
+        // 邮箱 空值判断
+        if (StringUtils.isBlank(user.getEmail())) {
+            map.put("emailMsg", "邮箱不能为空！");
+            return map;
+        }
+
+        // 用户名 重复值判断
+        User u = userMapper.selectByName(user.getUsername());
+        if (u != null) {
+            map.put("usernameMsg", "用户名已存在！");
+            return map;
+        }
+
+        // 邮箱 重复值判断
+        u = userMapper.selectByEmail(user.getEmail());
+        if (u != null) {
+            map.put("emailMsg", "邮箱已存在！");
+            return map;
+        }
+
+        // 注册用户
+        user.setSalt(CommunityUtil.generateUUID().substring(0, 5));
+        user.setPassword(CommunityUtil.md5(user.getPassword()) + user.getSalt());
+        user.setType(0);
+        user.setStatus(0);
+        user.setActivationCode(CommunityUtil.generateUUID());
+        user.setCreateTime(new Date());
+        user.setHeaderUrl(String.format("https://images.nowcoder.com/head/%dt.png", new Random().nextInt(1000)));
+        // 向数据库中插入user用户
+        userMapper.insertUser(user);
+
+        // 发送激活链接
+        Context context = new Context();
+        context.setVariable("email", user.getEmail());
+        // htttp://localhost:8080/community/activation/101/activationCode
+        String url = domain + contextPath + "/activation/" + user.getId() + "/" + user.getActivationCode();
+        context.setVariable("url", url);
+        String content = templateEngine.process("/mail/activation", context);
+        mailClient.sendMail(user.getEmail(), "激活账号", content);
+
+        return map;
+    }
+
+    /**
+     * 方法功能：判断当前用户激活状态
+     * @param userId
+     * @param activationCode
+     * @return
+     */
+    public int activation(int userId, String activationCode) {
+        User user = userMapper.selectById(userId);
+        if (user.getStatus() == 1) {
+            return ACTIVATION_REPEAT;
+        } else if (user.getActivationCode().equals(activationCode)) {
+            userMapper.updateStatus(userId, 1);
+            return ACTIVATION_SUCCESS;
+        } else {
+            return ACTIVATION_FAILURE;
+        }
+    }
 }
